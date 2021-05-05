744028296e06cdf3f7c6584695a95415dbc976ff
Your Name <you@example.com>
2021-05-05 11:50:24 +0200

    Add edit and grade button.

--- src/app/image-annotator/leaflet/leaflet.component.ts
+++ src/app/image-annotator/leaflet/leaflet.component.ts
@@ -16,18 +16,23 @@ import { LeafletService } from './leaflet.service';
 export class LeafletComponent implements OnInit, AfterViewInit {
   @Input() bioImageInfo!: ImageInfo;
   leafletContainerCssHeight!: string;
+  showBaseMap: boolean = false;
+  private selectedMaskId: string | null = null;
   private baseMap!: L.Map;
   private maskMap!: L.Map;
   private geoJsonLayer!: L.GeoJSON;
+  private maskControl!: L.Control;
+  private editMaskControl!: L.Control;
+  private gradeMaskControl!: L.Control;
+  private finishActionControl!: L.Control;
   private sw!: L.PointTuple;
   private ne!: L.PointTuple;
   private swMax!: L.PointTuple;
   private neMax!: L.PointTuple;
   private readonly tileSize: number = 128;
   private maxNativeZoom!: number;
-
-  showBaseMap: boolean = false;
-  private selectedMaskId: string | null = null;
+  private maskEditModeEnabled: boolean = false;
+  private maskGradeModeEnabled: boolean = false;
 
   constructor(
     private apiService: ApiService,
@@ -38,10 +43,7 @@ export class LeafletComponent implements OnInit, AfterViewInit {
 
   ngOnInit(): void {
     this.maskManagerService.maskSelected$.subscribe(
-      id => {
-        this.selectedMaskId = id;
-        this.updateGeoJson();
-      }
+      id => this.handleMaskChange(id)
     )
 
     this.leafletContainerCssHeight = `calc(100% - ${this.headerService.headerHeight}px)`;
@@ -121,8 +123,12 @@ export class LeafletComponent implements OnInit, AfterViewInit {
     const splitScreenControl = this.createSplitScreenControl();
     splitScreenControl.addTo(this.maskMap);
 
-    const maskControl = this.createMaskControl();
-    maskControl.addTo(this.maskMap);
+    this.maskControl = this.createMaskControl();
+    this.maskControl.addTo(this.maskMap);
+
+    this.editMaskControl = this.createEditMaskControl();
+    this.gradeMaskControl = this.createGradeMaskControl();
+    this.finishActionControl = this.createActionOperationControl();
 
     const imageNameControl = this.leafletService.createTextControl(this.bioImageInfo.originalName, 'bottomleft');
     imageNameControl.addTo(this.maskMap);
@@ -147,15 +153,113 @@ export class LeafletComponent implements OnInit, AfterViewInit {
     return this.leafletService.createButtonControl(button, 'topleft');
   }
 
-  private updateGeoJson(): void {
+  private createEditMaskControl(): L.Control {
+    const button: HTMLElement = L.DomUtil.create('a');
+    button.onclick = () => this.startMaskEditMode();
+    button.setAttribute('title', 'Edit');
+    button.style.backgroundImage = 'url("/assets/edit.png")';
+    button.style.backgroundSize = '24px 24px';
+    return this.leafletService.createButtonControl(button, 'topleft');
+  }
+
+  private createGradeMaskControl(): L.Control {
+    const button: HTMLElement = L.DomUtil.create('a');
+    button.onclick = () => this.startMaskGradeMode();
+    button.setAttribute('title', 'Grade');
+    button.style.backgroundImage = 'url("/assets/grade.png")';
+    button.style.backgroundSize = '24px 24px';
+    return this.leafletService.createButtonControl(button, 'topleft');
+  }
+
+  private createActionOperationControl(): L.Control {
+    const button: HTMLElement = L.DomUtil.create('a');
+    button.onclick = () => {
+      if (this.maskEditModeEnabled) {
+        this.finishMaskEditMode();
+      } else {
+        this.finishMaskGradeMode();
+      }
+    }
+    button.setAttribute('title', 'Finish');
+    button.style.backgroundImage = 'url("/assets/done.png")';
+    button.style.backgroundSize = '24px 24px';
+    return this.leafletService.createButtonControl(button, 'topleft');
+  }
+
+  private updateControls() {
+    this.maskControl.remove();
+    this.editMaskControl.remove();
+    this.gradeMaskControl.remove();
+    this.finishActionControl.remove();
+
+    if (!this.maskEditModeEnabled && !this.maskGradeModeEnabled) {
+      this.maskControl.addTo(this.maskMap);
+    }
+
+    if (this.selectedMaskId !== null && !this.maskGradeModeEnabled) {
+      this.editMaskControl.addTo(this.maskMap);
+    }
+
+    if (this.selectedMaskId !== null && !this.maskEditModeEnabled) {
+      this.gradeMaskControl.addTo(this.maskMap);
+    }
+
+    if (this.maskEditModeEnabled || this.maskGradeModeEnabled) {
+      this.finishActionControl.addTo(this.maskMap);
+    }
+  }
+
+  private handleMaskChange(maskId: string | null): void {
+    this.selectedMaskId = maskId;
+    this.updateGeoJson(maskId);
+    this.updateControls();
+  }
+
+  private updateGeoJson(maskId: string | null): void {
     this.geoJsonLayer.clearLayers();
 
-    if (this.selectedMaskId !== null) {
-      this.apiService.fetchGeoJson(this.bioImageInfo.id, this.selectedMaskId!).subscribe(
+    if (maskId !== null) {
+      this.apiService.fetchGeoJson(this.bioImageInfo.id, maskId!).subscribe(
         geoJsonPolygons => this.geoJsonLayer.addData(geoJsonPolygons as any),
         error => window.alert('Failed to retrieve polygons from server!')
       )
     }
+  }
+
+  private startMaskEditMode() {
+    if (this.maskEditModeEnabled) {
+      return
+    }
+
+    this.maskEditModeEnabled = true;
+    this.updateControls();
+  }
+
+  private finishMaskEditMode() {
+    if (!this.maskEditModeEnabled) {
+      return
+    }
+
+    this.maskEditModeEnabled = false;
+    this.updateControls();
+  }
+
+  private startMaskGradeMode() {
+    if (this.maskGradeModeEnabled) {
+      return
+    }
+
+    this.maskGradeModeEnabled = true;
+    this.updateControls();
+  }
+
+  private finishMaskGradeMode() {
+    if (!this.maskGradeModeEnabled) {
+      return
+    }
+
+    this.maskGradeModeEnabled = false;
+    this.updateControls();
   }
 
   private toLatLng(point: L.PointTuple): L.LatLng {
